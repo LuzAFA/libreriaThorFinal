9cf1159efb3ed33b16e9e30e7b7041d586c20bd1
luz fernandez <luz.fernandez01@uptc.edu.co>
2019-05-24 12:06:32 -0500

    falta  completar eventos y home

--- js/content.js
+++ js/content.js
@@ -19,7 +19,7 @@ let data = {
             datos.comics.forEach((libro) => {
 
                 if (libro.recomendado) {
-                nana++;
+                    nana++;
                     tabs += `
                     <div class="flip_card_container ">
                 <div class="flip_card_sub_container">
@@ -43,43 +43,53 @@ let data = {
             return doc;
         },
     },
+    "contenido": {
+        "estilosEspesificos": function () {
+            return fs.readFileSync('../css/recomendados.css').toString();
+        },
+        "contenidoPagina": function () {
+            let doc = fs.readFileSync('../html/recomendados.html').toString();
+            let tabs = "";
+            let datos = JSON.parse(fs.readFileSync('../data/libros.json', {encoding: 'utf-8'}));
+
+            console.log(datos.comics.length);
+
+            var nana = 0;
+            datos.comics.forEach((libro) => {
+                nana++;
+                tabs += `
+                    <div class="flip_card_container ">
+                <div class="flip_card_sub_container">
+
+                    <div class="flip_card_front">
+                        <img src=${libro.caratula} alt="Avatar"
+                             style="width:250px;height:300px;">
+                    </div>
+                    <div class="flip_card_back">
+                        <h1>${libro.Titulo}</h1>
+                        <p class="cardProfesion">${libro.Genero}</p>
+                        <p>${libro.sinopsis}
+                        </p><br><br><br>                   
+                    </div>
+                </div>
+            </div>
+             `
+            });
+            doc = remplaceContent(doc, {"tarjetas": tabs});
+            return doc;
+        },
+    },
     "eventos": {
+        "estilosEspesificos": function () {
+            return fs.readFileSync('../css/eventos.css').toString();
+        },
         "contenidoPagina": function () {
             return fs.readFileSync('../html/eventos.html').toString();
         }
     },
     "busqueda": {
-        "contenidoPagina": function () {
-            let doc = fs.readFileSync('../html/busqueda.html').toString();
-            let tabs = "";
-            let datos = JSON.parse(fs.readFileSync('../dataContent/comics.json', {encoding: 'utf-8'}));
-            datos.comics.forEach((libro) => {
-                tabs += `
-           <div class="flip_card_container">
-                <div class="flip_card_sub_container">
-                    <div class="flip_card_front">
-                        <img src="${libro.link}" alt="Avatar"
-                             style="width:250px;height:300px;" ruta="modal.html">
-                    </div>
-                    <div class="flip_card_back">
-                        <h1 class="tituloLibro">${libro.caratula}</h1>
-                        <p class="precioLibro">${libro.precio}</p>
-                        <button type="button">${libro.sinopsis}</button>
-                        <br><br>
-                        <form class="add-to-cart" action="cart.html" method="post">
-                            <div>
-                                <label for="qty-1">Cantidad</label>
-                                <input type="text" name="qty-1" id="qty-1" class="qty" value="1"/>
-                            </div>
-                            <p><input type="submit" value="Agregar al carrito" class="btn"/></p>
-                        </form>
-                    </div>
-                </div>
-            </div>`
-            });
-            doc = remplaceContent(doc, {"tarjetas": tabs});
-            return doc;
-        }
+        "estilosEspesificos": "",
+        "contenidoPagina": "pagina del buscador"
     }
 }
 
